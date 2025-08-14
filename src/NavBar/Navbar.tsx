98f4e963a9288e31a150baf8137bbe0e45d299b8
Dark <anakingobixtch@gmail.com>
2025-08-15 00:16:01 +0500

    Navbar Compleated For PC

--- src/NavBar/Navbar.tsx
+++ src/NavBar/Navbar.tsx
@@ -9,27 +9,32 @@ export const Navbar = () => {
         <div className="nav_inner">
           <div className="nav_left">
             <div className="nav_left_content">
-                <h2>KNIGHT ELECTRONICS</h2>
-                <small>SOMETHING YOU CAN TRUST</small>
+              <h2>KNIGHT ELECTRONICS</h2>
+              <small>SOMETHING YOU CAN TRUST</small>
             </div>
           </div>
           <div className="nav_right">
             <div className="nav_right_content">
-                <ul>
-                    <li>
-                        <Link to="/">HOME</Link>
-                    </li>
-                    <li>
-                        <Link to="/">PRODUCTS</Link>
-                    </li>
-                    <li>
-                        <Link to="/">SERVICES</Link>
-                    </li>
-                    <li>
-                        <Link to="/">CONTACT US</Link>
-                    </li>
-                </ul>
+              <ul>
+                <li>
+                  <Link to="/">HOME</Link>
+                </li>
+                <li>
+                  <Link to="/">PRODUCTS</Link>
+                </li>
+                <li>
+                  <Link to="/">SERVICES</Link>
+                </li>
+                <li>
+                  <Link to="/">CONTACT US</Link>
+                </li>
+              </ul>
             </div>
+          </div>
+
+          <div className="log_option">
+            <button>LOGIN / SIGN UP</button>
+            {/* <button>SIGN UP</button> */}
           </div>
         </div>
       </nav>
