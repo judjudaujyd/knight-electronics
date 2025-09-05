2059662d86040539aa4951c8cb9d95afc1fedda7
Dark <anakingobixtch@gmail.com>
2025-09-05 16:03:52 +0500

    Fixed

---
+++ src/components/SignOptions/SignOptionsApi.tsx
@@ -0,0 +1,17 @@
+type UserData = {
+    firstName : string,
+    lastName : string ,
+    phoneNumber : string ,
+    email : string ,
+    password : string,
+    confirmPassword : string
+}
+
+
+const signUpUser = async() => {
+
+
+
+
+
+}
